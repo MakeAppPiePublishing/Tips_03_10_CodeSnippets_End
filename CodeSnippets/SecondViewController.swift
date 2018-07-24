5435d95d7a7020418d7d3a4be4285da6e8ec0440
Steve Lipton <stevelipton@brk-16892.local>
2018-07-24 07:46:06 -0500

    First commit End state

--- CodeSnippets/SecondViewController.swift
+++ CodeSnippets/SecondViewController.swift
@@ -30,8 +30,9 @@ class SecondTableViewController:UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
     }
     
 }
+
 
